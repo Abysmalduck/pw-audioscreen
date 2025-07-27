5837c4c1735e7d9d874749062683732b0c310a0a
Timofey Dehtyar <dehtyarta@gmail.com>
2025-07-27 11:58:08 +0300

    Добавил блок try catch

--- audio_capture.py
+++ audio_capture.py
@@ -12,64 +12,71 @@ DEBUG_LOGS = False
 blacklist_list = ["alsa", "bluez", "Midi", "pipewire", "Mumble", "Discord", "kwin_wayland", "CompletedProcess", "stderr", "Firefox:monitor", "Playback", "rnnoise", "Capture", "monitor"]
 
 while True:
-    log_buffer = ""
+    try:
+        log_buffer = ""
 
-    raw_list = (str(subprocess.run(["pw-link", "-oI"], capture_output=True, timeout=1)).split("\\n"))
+        raw_list = (str(subprocess.run(["pw-link", "-oI"], capture_output=True, timeout=1)).split("\\n"))
 
-    log_buffer += "Raw list: " + str(raw_list) + "\n\n"
+        log_buffer += "Raw list: " + str(raw_list) + "\n\n"
 
-    allowed_list = []
-    not_allowed_list = []
+        allowed_list = []
+        not_allowed_list = []
 
-    for entry in raw_list:
-        not_allowed_flag = False
-        for backlist_entry in blacklist_list:
-            if backlist_entry in entry:
-                not_allowed_flag = True
-                not_allowed_list.append(entry)
-                break
-        
-        if not_allowed_flag == False:
-            allowed_list.append(entry)
+        for entry in raw_list:
+            not_allowed_flag = False
+            for backlist_entry in blacklist_list:
+                if backlist_entry in entry:
+                    not_allowed_flag = True
+                    not_allowed_list.append(entry)
+                    break
+            
+            if not_allowed_flag == False:
+                allowed_list.append(entry)
 
-    log_buffer += "Allowed: " + str(allowed_list) + "\n\n"
-    log_buffer += "Not Allowed: " + str(not_allowed_list) + "\n\n"
-    #connecting
+        log_buffer += "Allowed: " + str(allowed_list) + "\n\n"
+        log_buffer += "Not Allowed: " + str(not_allowed_list) + "\n\n"
+        #connecting
 
-    for app in allowed_list:
-        app_id = extract_leading_number(app)
-        log_buffer += str(app_id) + " : "
-        try:
-            if str(app).endswith("FR"):  
-                log_buffer += str(subprocess.run(["pw-link", str(app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
-                pass
-            else:
-                log_buffer += str(subprocess.run(["pw-link", str(app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
-                pass
-        except:
-            log_buffer += "timeout"
+        for app in allowed_list:
+            app_id = extract_leading_number(app)
+            log_buffer += str(app_id) + " : "
+            try:
+                if str(app).endswith("FR"):  
+                    log_buffer += str(subprocess.run(["pw-link", str(app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
+                    pass
+                else:
+                    log_buffer += str(subprocess.run(["pw-link", str(app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
+                    pass
+            except:
+                log_buffer += "timeout"
 
-        log_buffer += "\n\n"
+            log_buffer += "\n\n"
 
-    #disconnecting not allowed
+        #disconnecting not allowed
 
-    for app in not_allowed_list:
-        app_id = extract_leading_number(app)
-        log_buffer += str(app_id) + " : "
-        try:
-            if str(app).endswith("FR"):
-                log_buffer += str(subprocess.run(["pw-link", "-d", str(app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
-                pass
-            else:
-                log_buffer += str(subprocess.run(["pw-link", "-d", str(app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
-                pass
-        except:
-            log_buffer += "timeout"
-        log_buffer += "\n\n"
+        for app in not_allowed_list:
+            app_id = extract_leading_number(app)
+            log_buffer += str(app_id) + " : "
+            try:
+                if str(app).endswith("FR"):
+                    log_buffer += str(subprocess.run(["pw-link", "-d", str(app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
+                    pass
+                else:
+                    log_buffer += str(subprocess.run(["pw-link", "-d", str(app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
+                    pass
+            except:
+                log_buffer += "timeout"
+            log_buffer += "\n\n"
 
-    if DEBUG_LOGS:
-        with open("log.txt", 'w') as file:
-            file.write(log_buffer)
+        if DEBUG_LOGS:
+            with open("log.txt", 'w') as file:
+                file.write(log_buffer)
 
 
-    time.sleep(0.15)
+        time.sleep(0.15)
+    except FileNotFoundError:
+        print("Looks like pw-link is not installed, maybe system is updating, maybe pipewire is not installed, anyway, restarting script..")
+        time.sleep(4)
+    except:
+        print("Unknown error, restarting script")
+        time.sleep(4)
