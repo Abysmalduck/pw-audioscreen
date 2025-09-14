9feedf614c0090b173f406f3ebf97650672ee765
Timofey Dehtyar <dehtyarta@gmail.com>
2025-09-14 23:09:15 +0300

    Создание виртуального микрофона

--- audio_capture.py
+++ audio_capture.py
@@ -3,9 +3,11 @@ import time
 import re
 import os
 
+
 def extract_leading_number(line):
     match = re.match(r'\s*(\d+)', line)
     return int(match.group(1)) if match else None
+
 
 def get_node_id(add_info):
     match = re.search(r'node\.id = \"[0-9]+\"', add_info)
@@ -15,16 +17,27 @@ def get_node_id(add_info):
 
     return None
 
-DEVICE_NAME = "pipewire-screenaudio"
+
+
 DEBUG_LOGS = False
 
-blacklist_list = ["alsa", "bluez", "Midi", "pipewire", "Mumble", "Discord", "kwin_wayland", "CompletedProcess", "stderr", "Playback", "rnnoise", "Capture", "monitor", "Screego", "Vite", "ClownShare"]
+blacklist_list = ["alsa", "bluez", "Midi", "pipewire", "Mumble", "Discord", "kwin_wayland",
+                  "CompletedProcess", "stderr", "Playback", "rnnoise", "Capture", "monitor", "Screego", "Vite", "ClownShare"]
+
+DEVICE_NAME = "clownshare-mic"
+
+# Creating virtual mic
+pw_input_info = str(subprocess.run(["pw-link", "-i"], capture_output=True, timeout=1))
+if DEVICE_NAME not in pw_input_info:
+    print(f"No {DEVICE_NAME}, creating one...")
+    os.system("pw-cli create-node adapter '{ \"factory.name\": \"support.null-audio-sink\", \"node.name\": \"clownshare-mic\", \"node.description\": \"ClownShare Virtual Microphone\", \"media.class\": \"Audio/Source/Virtual\", \"audio.position\": \"[ FL FR ]\", \"object.linger\": true}'")
 
 while True:
     try:
         log_buffer = ""
 
-        raw_list = (str(subprocess.run(["pw-link", "-oI"], capture_output=True, timeout=1)).split("\\n"))[2:-1]
+        raw_list = (str(subprocess.run(
+            ["pw-link", "-oI"], capture_output=True, timeout=1)).split("\\n"))[2:-1]
 
         log_buffer += "Raw list: " + str(raw_list) + "\n\n"
 
@@ -36,11 +49,14 @@ while True:
 
             try:
                 app_id = extract_leading_number(entry)
-                additional_entry_info = str(subprocess.run(["pw-cli", "info", str(app_id)] , capture_output=True, timeout=1))
+                additional_entry_info = str(subprocess.run(
+                    ["pw-cli", "info", str(app_id)], capture_output=True, timeout=1))
                 node_id = get_node_id(additional_entry_info)
-                additional_node_info = str(subprocess.run(["pw-cli", "info", str(node_id)] , capture_output=True, timeout=1))
+                additional_node_info = str(subprocess.run(
+                    ["pw-cli", "info", str(node_id)], capture_output=True, timeout=1))
 
-                log_buffer += str(app_id) + " : additional_node_info : " + additional_node_info + "\n\n"
+                log_buffer += str(app_id) + " : additional_node_info : " + \
+                    additional_node_info + "\n\n"
 
                 if "Screego" in additional_node_info:
                     not_allowed_flag = True
@@ -53,44 +69,48 @@ while True:
                     not_allowed_list.append(entry)
             except FileNotFoundError:
                 pass
-            
+
             if not_allowed_flag == False:
                 for blacklist_entry in blacklist_list:
-                    
+
                     if blacklist_entry in entry:
                         not_allowed_flag = True
                         not_allowed_list.append(entry)
                         break
-                
+
             if not_allowed_flag == False:
                 allowed_list.append(entry)
 
         log_buffer += "Allowed: " + str(allowed_list) + "\n\n"
         log_buffer += "Not Allowed: " + str(not_allowed_list) + "\n\n"
-        #connecting allowed array
+        # connecting allowed array
 
         for app in allowed_list:
             app_id = extract_leading_number(app)
             log_buffer += str(app_id) + " : "
             try:
-                if str(app).endswith("FR"):  
-                    log_buffer += str(subprocess.run(["pw-link", str(app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
+                if str(app).endswith("FR"):
+                    log_buffer += str(subprocess.run(["pw-link", str(
+                        app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
                     pass
                 else:
-                    log_buffer += str(subprocess.run(["pw-link", str(app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
+                    log_buffer += str(subprocess.run(["pw-link", str(
+                        app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
                     pass
             except:
                 log_buffer += "timeout"
 
             log_buffer += "\n\n"
 
-        #disconnecting not allowed array
+        # disconnecting not allowed array
         for app in not_allowed_list:
             app_id = extract_leading_number(app)
             log_buffer += str(app_id) + " : "
             try:
-                log_buffer += str(subprocess.run(["pw-link", "-d", str(app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
-                log_buffer += str(subprocess.run(["pw-link", "-d", str(app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
+                log_buffer += str(subprocess.run(["pw-link", "-d", str(
+                    app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
+                log_buffer += str(subprocess.run(["pw-link", "-d", str(
+                    app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
             except:
                 log_buffer += "timeout"
             log_buffer += "\n\n"
@@ -98,7 +118,6 @@ while True:
         if DEBUG_LOGS:
             with open("log.txt", 'w') as file:
                 file.write(log_buffer)
-
 
         time.sleep(0.5)
     except FileNotFoundError:
