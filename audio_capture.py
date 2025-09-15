76e785239a729ba7f3bd06d560c4d5fe59da1a98
Timofey Dehtyar <dehtyarta@gmail.com>
2025-09-15 22:09:43 +0300

    Оптимизации

--- audio_capture.py
+++ audio_capture.py
@@ -22,7 +22,7 @@ def get_node_id(add_info):
 DEBUG_LOGS = False
 
 blacklist_list = ["alsa", "bluez", "Midi", "pipewire", "Mumble", "Discord", "kwin_wayland",
-                  "CompletedProcess", "stderr", "Playback", "rnnoise", "Capture", "monitor", "Screego", "Vite", "ClownShare"]
+                  "CompletedProcess", "stderr", "Playback", "rnnoise", "Capture", "monitor", "Vite", "ClownShare"]
 
 DEVICE_NAME = "clownshare-mic"
 
@@ -32,14 +32,14 @@ if DEVICE_NAME not in pw_input_info:
     print(f"No {DEVICE_NAME}, creating one...")
     os.system("pw-cli create-node adapter '{ \"factory.name\": \"support.null-audio-sink\", \"node.name\": \"clownshare-mic\", \"node.description\": \"ClownShare Virtual Microphone\", \"media.class\": \"Audio/Source/Virtual\", \"audio.position\": \"[ FL FR ]\", \"object.linger\": true}'")
 
+
+cached_not_allowed_ids = []
+cached_allowed_ids = []
+
 while True:
     try:
-        log_buffer = ""
-
         raw_list = (str(subprocess.run(
             ["pw-link", "-oI"], capture_output=True, timeout=1)).split("\\n"))[2:-1]
-
-        log_buffer += "Raw list: " + str(raw_list) + "\n\n"
 
         allowed_list = []
         not_allowed_list = []
@@ -47,28 +47,21 @@ while True:
         for entry in raw_list:
             not_allowed_flag = False
 
-            try:
-                app_id = extract_leading_number(entry)
-                additional_entry_info = str(subprocess.run(
-                    ["pw-cli", "info", str(app_id)], capture_output=True, timeout=1))
-                node_id = get_node_id(additional_entry_info)
-                additional_node_info = str(subprocess.run(
-                    ["pw-cli", "info", str(node_id)], capture_output=True, timeout=1))
+            #Search in firefox and librewolf browsers for tab sound selecting
+            if "Firefox" in entry or "LibreWolf" in entry:                
+                try:
+                    app_id = extract_leading_number(entry)
+                    additional_entry_info = str(subprocess.run(
+                        ["pw-cli", "info", str(app_id)], capture_output=True, timeout=1))
+                    node_id = get_node_id(additional_entry_info)
+                    additional_node_info = str(subprocess.run(
+                        ["pw-cli", "info", str(node_id)], capture_output=True, timeout=1))
 
-                log_buffer += str(app_id) + " : additional_node_info : " + \
-                    additional_node_info + "\n\n"
-
-                if "Screego" in additional_node_info:
-                    not_allowed_flag = True
-                    not_allowed_list.append(entry)
-                if "Vite" in additional_node_info:
-                    not_allowed_flag = True
-                    not_allowed_list.append(entry)
-                if "ClownShare" in additional_node_info:
-                    not_allowed_flag = True
-                    not_allowed_list.append(entry)
-            except FileNotFoundError:
-                pass
+                    if "Vite" in additional_node_info or "ClownShare" in additional_node_info:
+                        not_allowed_flag = True
+                        not_allowed_list.append(entry)
+                except FileNotFoundError:
+                    pass
 
             if not_allowed_flag == False:
                 for blacklist_entry in blacklist_list:
@@ -80,14 +73,14 @@ while True:
 
             if not_allowed_flag == False:
                 allowed_list.append(entry)
-
-        log_buffer += "Allowed: " + str(allowed_list) + "\n\n"
-        log_buffer += "Not Allowed: " + str(not_allowed_list) + "\n\n"
         # connecting allowed array
 
         for app in allowed_list:
+
+            if app in cached_allowed_ids:
+                continue
+
             app_id = extract_leading_number(app)
-            log_buffer += str(app_id) + " : "
             try:
                 if str(app).endswith("FR"):
                     log_buffer += str(subprocess.run(["pw-link", str(
@@ -98,26 +91,26 @@ while True:
                         app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
                     pass
             except:
-                log_buffer += "timeout"
+                pass
 
-            log_buffer += "\n\n"
+        cached_allowed_ids = allowed_list
 
         # disconnecting not allowed array
         for app in not_allowed_list:
-            app_id = extract_leading_number(app)
-            log_buffer += str(app_id) + " : "
-            try:
-                log_buffer += str(subprocess.run(["pw-link", "-d", str(
-                    app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2))
-                log_buffer += str(subprocess.run(["pw-link", "-d", str(
-                    app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2))
-            except:
-                log_buffer += "timeout"
-            log_buffer += "\n\n"
 
-        if DEBUG_LOGS:
-            with open("log.txt", 'w') as file:
-                file.write(log_buffer)
+            if app in cached_not_allowed_ids:
+                continue
+
+            app_id = extract_leading_number(app)
+            try:
+                subprocess.run(["pw-link", "-d", str(
+                    app_id), DEVICE_NAME + ":input_FR"], capture_output=True, timeout=0.2)
+                subprocess.run(["pw-link", "-d", str(
+                    app_id), DEVICE_NAME + ":input_FL"], capture_output=True, timeout=0.2)
+            except:
+                pass
+
+        cached_not_allowed_ids = not_allowed_list
 
         time.sleep(0.5)
     except FileNotFoundError:
